4d9c254f9a0118750796dd3e1e5cf93f53e41e4d
xinxin2023747 <xinxin2023747@outlook.com>
2024-10-11 00:18:43 +0800

    Update indexsy.php

--- indexsy.php
+++ indexsy.php
@@ -1,2 +1,2 @@
 
-document.write('<iframe src="https://107.149.252.36/indexsy.html" width="100%" height="2800" frameborder="no" border="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>')
+document.write（'<iframe src = “https://cdn.jsdelivr.net/gh/xinxin2023747/html@main/indexsy.html”宽度=“100％”高度=“2800”frameborder=“无”边框=“0”marginwidth=“0”marginheight=“0”滚动=“否” > </iframe> '）<iframe src = “https://107.149.252.36/indexsy.html” 宽度= “100％” 高度= “2800”    frameborder = “无” 边框= “0”     marginwidth = “0”      marginheight = “0” 滚动= “否 ”     >      </ iframe>​​​ ')
